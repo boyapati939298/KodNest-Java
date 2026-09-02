1a432522323ada1a35a258af021e1d43b3a9613d
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-02 15:20:43 +0530

    Tyring to jum the folder Not worked

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,6 +4,7 @@ public class WelcomeToJava {
        System.out.println("Hello");
        System.out.println("Welcome to java");
        System.out.println("I am a programmer");
+       System.out.println("Trying Something New");
        
     }
 }
