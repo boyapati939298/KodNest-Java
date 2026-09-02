1345cb195573256034e9a25b68ae034f788ccb91
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-02 14:43:59 +0530

    Added new Statements

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,5 +2,8 @@ public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
        System.out.println("Hello");
+       System.out.println("Welcome to java");
+       System.out.println("I am a programmer");
+       
     }
 }
