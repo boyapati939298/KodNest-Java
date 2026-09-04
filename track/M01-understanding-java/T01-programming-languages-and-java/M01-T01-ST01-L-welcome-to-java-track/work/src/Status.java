22335ac6213fa030cf4e27cd7d5dd00d6b3265fe
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-04 12:29:58 +0530

    Git Status Of our Repository

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Status.java
@@ -0,0 +1,10 @@
+public class Status {
+
+    public static void main(String[] args) {
+        // Display the repository cloning status.
+        System.out.println("Repository: Cloned Sucessfully");
+
+        // Display Whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
