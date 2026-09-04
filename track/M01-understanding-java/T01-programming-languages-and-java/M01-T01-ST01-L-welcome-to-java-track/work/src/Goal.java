61ea451ef89bfa1012c5e8c0cac98f64772821b0
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-04 12:19:28 +0530

    Placement Goal

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Goal.java
@@ -0,0 +1,9 @@
+public class Goal {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("---MY PLACEMENT GOAL ---");
+        System.out.println("Role: Software Developer");
+        System.out.println("Target: Get Placed");
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
