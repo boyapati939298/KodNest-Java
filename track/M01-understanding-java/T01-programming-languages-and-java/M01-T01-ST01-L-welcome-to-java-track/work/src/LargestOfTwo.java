f7be5a6294d6d37a0a93458970fb81db174e766e
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-06 12:18:06 +0530

    Largest of Two Numbers

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/LargestOfTwo.java
@@ -0,0 +1,12 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 26;
+        int b = 35;
+        
+        if (a > b) {
+            System.out.println("A is largest:" +a);
+        } else {
+            System.out.println("B is largest:" +b);
+        }
+    }
+}
