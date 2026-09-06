e5452fa025e2087749832ce7dc645ed2a589c6ec
boyapati939298 <pradeepkumarboyapati4@gmail.com>
2026-09-06 12:21:35 +0530

    Check Number Status Positive , Negative or Zero

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/PositiveNegativeZero.java
@@ -0,0 +1,14 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+
+        int number = 8;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
